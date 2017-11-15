58e66e1b8fbaaff25a5e155d2eee65abf4522219
giuven95 <33382878+giuven95@users.noreply.github.com>
2017-11-15 03:34:54 +0100

    Change jump search to recursive (style)

--- algos/searching/jump_search.py
+++ algos/searching/jump_search.py
@@ -2,26 +2,22 @@ from math import sqrt
 
 
 # the list has to be sorted already
-def jump_search(l, key):
-    start = 0
-    end = len(l)
-    
-    right = int(sqrt(end))
-    mid = min(right, end)
-    while l[mid - 1] < key:
-        start = right
-        right += int(sqrt(end))
-        mid = min(right, end)
-        if end <= start:
-            return None
-
-    end = mid
-    while l[start] < key:
-        start += 1
-        if end <= start:
-            return None
+def jump_search(l, key, start=0, end=None):
+    if end is None:
+        end = len(l)
+    if end <= start:
+        return None
 
     if l[start] == key:
         return start
-    else:
-        return None
+    start += 1    
+
+    right = start + int(sqrt(end - start))
+    mid = min(right, end)
+    if l[mid - 1] < key:
+        return jump_search(l, key, mid, end)
+
+    end = mid
+    while start < end and l[start] < key:
+        start += 1
+    return jump_search(l, key, start, end)
